cdc4a58949062e41dda558cd4606fe80e32f22c9
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-23 11:20:26 +0530

    feat:card component created.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Card from "@/components/Card";
 export default function Home() {
-  return <div>Pokemon.</div>;
+  return (
+    <div className=" mx-40 mt-8 grid grid-cols-5 gap-4">
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      <Card id={1} name="tushal"/>
+      
+    </div>
+  );
 }

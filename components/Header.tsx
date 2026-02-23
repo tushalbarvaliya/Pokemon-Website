0506c0efa21ac41c0dbb150e170b5fcc33fe9f55
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-23 16:38:43 +0530

    fix:make header responsive.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,33 @@
+// import Image from "next/image";
+// import React from "react";
+
+// import pokemon_Logo from "@/public/pokedex.png";
+// import Button from "./UI/Button";
+// import Link from "next/link";
+
+// const Header = () => {
+//   return (
+//     <header className="bg-stone-50 border-2">
+//       <nav className="flex sm:flex-row flex-col justify-between p-4 items-center gap-4">
+//         <Link href={"/"}>
+//           <Image src={pokemon_Logo} alt="pokemon logo" width={150}></Image>
+//         </Link>
+//         <div className="flex items-center gap-4  w-full sm:w-fit">
+//           <input
+//             type="text"
+//             placeholder="Search pokemon by id or name"
+//             className="border-2 outline-0 py-2 px-4 rounded flex-1"
+//           />
+//           <Button>Search</Button>
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
 import Image from "next/image";
 import React from "react";
 
@@ -8,15 +38,15 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="bg-stone-50 border-2">
-      <nav className="flex sm:flex-row flex-col justify-between p-4 items-center gap-4">
+      <nav className="flex sm:flex-row  flex-col justify-between p-4 items-center sm:gap-0 gap-2">
         <Link href={"/"}>
           <Image src={pokemon_Logo} alt="pokemon logo" width={150}></Image>
         </Link>
-        <div className="flex items-center gap-4  w-full sm:w-fit">
+        <div className="flex items-center gap-4 sm:justify-center w-full sm:w-fit">
           <input
             type="text"
             placeholder="Search pokemon by id or name"
-            className="border-2 outline-0 py-2 px-4 rounded flex-1"
+            className="sm:w-sm w-full border-2 outline-0 py-2 px-4 rounded"
           />
           <Button>Search</Button>
         </div>

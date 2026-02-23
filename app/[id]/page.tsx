ce08672ef633886882a4b08bfe85d4b25fbb4839
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-23 16:21:09 +0530

    fix:type is define and improve ftech code.

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from "react";
+
+import React, { FC, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { fetchPokemonById } from "../fetchPokemon";
@@ -8,7 +9,9 @@ import Image from "next/image";
 import SmallCard from "@/components/UI/SmallCard";
 import Type from "@/components/UI/Type";
 
-const page: React.FC = () => {
+const page: FC = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [src, setSrc] = useState("/heart-outline.svg");
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { id } = useParams();
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -18,21 +21,23 @@ const page: React.FC = () => {
   });
   const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`;
 
-  const [src,Setsrc] = useState('/heart-outline.svg')
-  function handelFav(){
-    if(src=='/heart-outline.svg'){
-      Setsrc('/heart-fill.svg')
-    }else{
-      Setsrc('/heart-outline.svg')
-    }    
+  // for fav icon change per click
+  function handelFav() {
+    if (src == "/heart-outline.svg") {
+      setSrc("/heart-fill.svg");
+    } else {
+      setSrc("/heart-outline.svg");
+    }
   }
   return (
     <>
       <h1 className="capitalize text-2xl font-semibold px-4">{data?.name}</h1>
       <div className="flex gap-2 px-4 my-2">
-        {data?.types.map((type) => {
-          return <Type key={type.type.name} name={type.type.name} />;
-        })}
+        {data?.types.map(
+          (type: { slot: number; type: { name: string; url: string } }) => {
+            return <Type key={type.type.name} name={type.type.name} />;
+          },
+        )}
         <div className="flex items-center" onClick={handelFav}>
           <Image src={src} width={30} height={30} alt="heart" />
         </div>
@@ -46,15 +51,21 @@ const page: React.FC = () => {
           ></SmallCard>
           <SmallCard text={"Height"} value={data?.height}></SmallCard>
           <SmallCard text={"weight"} value={`${data?.weight} Kg`}></SmallCard>
-          {data?.stats.map((state) => {
-            return (
-              <SmallCard
-                text={state.stat.name}
-                value={state.base_stat}
-                key={state.stat.name}
-              />
-            );
-          })}
+          {data?.stats.map(
+            (state: {
+              base_stat: number;
+              effort: number;
+              stat: { name: string; url: string };
+            }) => {
+              return (
+                <SmallCard
+                  text={state.stat.name}
+                  value={state.base_stat}
+                  key={state.stat.name}
+                />
+              );
+            },
+          )}
           <div className="col-start-6 row-start-1 row-span-2 col-span-3 flex justify-center">
             <Image
               src={url}
--- app/fetchPokemon.ts
+++ app/fetchPokemon.ts
@@ -1,8 +1,8 @@
-async function fetchPokemon({ offset = 0 }: { offset: number }) {
-  console.log(offset);
+async function fetchPokemon({ pageParam = 0 }: { pageParam: number }) {
+  console.log(pageParam);
 
   const res = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=50&offset=" + offset,
+    "https://pokeapi.co/api/v2/pokemon?limit=50&offset=" + pageParam,
   );
   if (!res.ok) {
     throw new Error("Data not Found");
